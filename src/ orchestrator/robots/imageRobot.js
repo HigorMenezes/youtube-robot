44d5d9944734d10492cda9b52d2f95c43a1d568d
Higor Menezes <higor_menezes96@hotmail.com.br>
2019-07-14 18:39:06 -0400

    basic research with google api

--- src/ orchestrator/robots/imageRobot.js
+++ src/ orchestrator/robots/imageRobot.js
@@ -1,8 +1,22 @@
 const google = require('googleapis');
 const stateRobot = require('./stateRobot');
 
+const googleConfig = require('../../configs/googleConfig');
+
+const customSearch = new google.customsearch_v1.Customsearch();
+
 async function imageRobot() {
   const content = stateRobot.load();
+
+  const response = await customSearch.cse.list({
+    auth: googleConfig.apiKey,
+    cx: googleConfig.searchEngineId,
+    q: 'Michael Jackson',
+    num: 2,
+  });
+
+  console.dir(response, { depth: null });
+  process.exit(0);
 }
 
 module.exports = imageRobot;
--- src/configs/googleConfig.js
+++ src/configs/googleConfig.js
@@ -4,6 +4,7 @@ const googleConfig = {
   clientId: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   apiKey: process.env.GOOGLE_API_KEY,
+  searchEngineId: process.env.GOOGLE_SEARCH_ENGINE_ID,
 };
 
 module.exports = googleConfig;

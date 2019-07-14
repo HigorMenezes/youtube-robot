985db37a005bb7fb9b6e8fe734c6f790965f8fbc
Higor Menezes <higor_menezes96@hotmail.com.br>
2019-07-14 18:50:07 -0400

    function to fetch google images by query

--- src/ orchestrator/robots/imageRobot.js
+++ src/ orchestrator/robots/imageRobot.js
@@ -5,17 +5,29 @@ const googleConfig = require('../../configs/googleConfig');
 
 const customSearch = new google.customsearch_v1.Customsearch();
 
-async function imageRobot() {
-  const content = stateRobot.load();
-
+async function fetchGoogleAndReturnImageLinks(query) {
   const response = await customSearch.cse.list({
     auth: googleConfig.apiKey,
     cx: googleConfig.searchEngineId,
-    q: 'Michael Jackson',
+    q: query,
+    searchType: 'image',
     num: 2,
   });
 
-  console.dir(response, { depth: null });
+  const imageUrls = response.data.items.map(item => {
+    return item.link;
+  });
+
+  return imageUrls;
+}
+
+async function imageRobot() {
+  const content = stateRobot.load();
+
+  console.dir(await fetchGoogleAndReturnImageLinks('Michael Jackson'), {
+    depth: null,
+  });
+
   process.exit(0);
 }
 
